d6bc9284674a21f0c59fb8affea347c2b8b402dc
Sagar Devops <135566555+Sagarsgsg@users.noreply.github.com>
2023-11-25 16:55:15 +0530

    Create dictionary-for-loop.py

---
+++ dictionary-for-loop.py
@@ -0,0 +1,14 @@
+#dictionory for loop #
+
+person = {'name' : 'sagu', 'age' : 30, 'city' : 'londan'}
+
+for key,value in person.items():
+    print(f'{key}:{value}')
+    
+#O/p:::--
+
+name:sagu
+age:30
+city:londan
+
+f--> 2 value is used in the string we use the letter f
