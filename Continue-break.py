d68397e14065e7d43db1f4224b9f036343bbe148
Sagar Devops <135566555+Sagarsgsg@users.noreply.github.com>
2023-11-25 16:26:18 +0530

    Create Continue-break.py

---
+++ Continue-break.py
@@ -0,0 +1,33 @@
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+for i in range(10):
+    if i == 5:
+       continue
+    print(i)
+
+
+
+O/P::-
+
+O/P::-
+
+0
+1
+2
+3
+4 it will break the loop once i=5 condition.
+
+O/P::-
+
+0
+1
+2
+3
+4 it will continue the loop by skiping once i=5 condition.
+6
+7
+8
+9
